599edbadf26cabd758a8982b79cf2b85b5c81227
Ernesto Celis <ecelis@tiempodevelopment.com>
2019-05-22 16:44:43 -0500

    iChanges

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MouseManager : MonoBehaviour
 {
@@ -10,8 +11,9 @@ public class MouseManager : MonoBehaviour
     // Swap cursors per object
     public Texture2D pointer;   // normal pointer
     public Texture2D target;    // Cursor for clickable objects, like world.
-    public Texture2D doorways;  // Cursor for doorways.
+    public Texture2D doorway;  // Cursor for doorways.
     public Texture2D combat;    // Cursor for combat actions.
+    public EventVector3 OnClickEnvironment;
 
     // Update is called once per frame
     void Update()
@@ -21,18 +23,46 @@ public class MouseManager : MonoBehaviour
         if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
         {
             bool door = false;
-            if(hit.collider.gameObject.tag == "Doorway")
+            bool item = false;
+            if (hit.collider.gameObject.tag == "Doorway")
             {
-                Cursor.SetCursor(doorways, new Vector2(16, 16), CursorMode.Auto);
+                Cursor.SetCursor(doorway, new Vector2(16, 16), CursorMode.Auto);
                 door = true;
-            } else
+            }
+            else if (hit.collider.gameObject.tag == "Item")
+            {
+                Cursor.SetCursor(combat, new Vector2(16, 16), CursorMode.Auto);
+            }
+            else
             {
                 Cursor.SetCursor(target, new Vector2(16, 16), CursorMode.Auto);
-
+                item = true;
+            }
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(door)
+                {
+                    Transform doorway = hit.collider.gameObject.transform;
+                    OnClickEnvironment.Invoke(doorway.position);
+                    Debug.Log("DOOR");
+                }
+                else if(item)
+                {
+                    Transform itemPos = hit.collider.gameObject.transform;
+                    OnClickEnvironment.Invoke(itemPos.position);
+                    Debug.Log("ITEM");
+                }
+                else
+                {
+                    OnClickEnvironment.Invoke(hit.point);
+                }
             }
         } else
         {
-            Cursor.SetCursor(pointer, new Vector2.zero, CursorMode.Auto);
+            Cursor.SetCursor(pointer, Vector2.zero, CursorMode.Auto);
         }
     }
 }
+
+[System.Serializable]
+public class EventVector3 : UnityEvent<Vector3> { }
